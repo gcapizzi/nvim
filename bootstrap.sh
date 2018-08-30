9d063d7751d29a4535b8e43f65cdef75b1b99477
Giuseppe Capizzi <g.capizzi@gmail.com>
2018-08-30 19:26:42 +0100

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+endpath="$HOME/.config/nvim"
+
+mkdir -p $endpath
+
+if [ -e $endpath/.git ] ; then
+    echo "> update repo"
+    cd $endpath && git pull
+else
+    echo "> clone repo"
+    git clone http://github.com/gcapizzi/nvim.git $endpath
+fi
+
+echo "> install vim-plug"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "> install vim-plug"
+nvim +PlugUpdate +qall
